b2db445155ed4b2c1a15127661a7ddcb0b917162
nechami_rapaport <nr272067@gmail.com>
2024-03-19 18:24:08 +0200

    praktykod5

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -1,8 +1,8 @@
-
+using Microsoft.EntityFrameworkCore;
+using TodoApi;
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
-
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
